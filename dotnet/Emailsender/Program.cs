39fe61f056523d62941ff03cdb97dbd7b1d267a2
Erishprajapati <149457413+Erishprajapati@users.noreply.github.com>
2024-07-25 07:59:23 +0545

    Add files via upload

---
+++ dotnet/Emailsender/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using system;
+using System.Collections.Generic;
+public interface IMessageSender
+{
+    void SendMessage(string message);`
+}
+
+public class EmailSender : IMessageSender
+{
+    public void SendMessage(string message)
+    {
+        Console.WriteLine($"Sending email with message: {message}");
+    }
+}
+
+public class SmsSender : IMessageSender
+{
+    public void SendMessage(string message)
+    {
+        Console.WriteLine($"Sending SMS with message: {message}");
+    }
+}
+
+public class MessageService
+{
+    private IMessageSender _messageSender;
+
+    public MessageService(IMessageSender messageSender)
+    {
+        _messageSender = messageSender;
+    }
+
+    public void Send(string message)
+    {
+        _messageSender.SendMessage(message);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<IMessageSender> messageSenders = new List<IMessageSender>
+        {
+            new EmailSender(),
+            new SmsSender()
+        };
+
+        foreach (var sender in messageSenders)
+        {
+            MessageService messageService = new MessageService(sender);
+            messageService.Send("Hello world!");
+        }
+    }
+}
